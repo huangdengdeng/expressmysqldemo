35d658d4d9455dd2083abbd05d51f308d47af2bc
huangdengdeng <744226174@qq.com>
2020-07-20 23:28:42 +0800

    add project

---
+++ conf/mysqlConf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mysql: {
+        host: '192.168.1.103',
+        user: 'root',
+        password: 'root123',
+        database:'express-mysql-demo',
+        // 最大连接数，默认为10
+        connectionLimit: 10
+    }
+};
